4b7c8dcf7a588c3d465200970d3665f2c6c56425
imudayveer <manshahia.uday@gmail.com>
2021-02-16 01:53:43 +0530

    Update copyright headers

--- helpers.py
+++ helpers.py
@@ -1,6 +1,6 @@
 # MIT License
 
-# Copyright (c) 2020 imudayveer
+# Copyright (C) 2020-2021 imudayveer
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
